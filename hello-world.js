1b44dc95a175b3d0ef507eee74671cee7e66d2d3
Abhinandan Ramaprasath <abhinandan.ramaprasath@shopify.com>
2016-08-14 18:33:02 -0400

    Some front page

--- hello-world.js
+++ hello-world.js
@@ -2,11 +2,12 @@ var express = require('express');
 var app = express();
 
 app.use('/static', express.static('public'));
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.send('<img src="/static/helloworld.gif"/>');
+  res.render('index', { title: 'The index page!' })
 });
 
 app.listen(19921, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 19921!');
 });
