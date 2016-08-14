926724dfcb196e0bd512364b05c426c53cc97712
Abhinandan Ramaprasath <abhinandan.ramaprasath@shopify.com>
2016-08-14 15:37:12 -0400

    Add pic

--- hello-world.js
+++ hello-world.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World2!');
+  res.send('<img src="/public/hello_world.gif"/>');
 });
 
 app.listen(19921, function () {
